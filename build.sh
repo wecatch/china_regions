aa30406e5a25c8d3def094b273c45ea538f00925
zhaoyongqiang <zhyq0826@gmail.com>
2017-03-11 16:12:07 +0800

    add build

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# git branch -D gh-pages
+# git push origin --delete gh-pages
+# git checkout -b gh-pages
+cd location-demo 
+pwd
+ember build --environment gh-pages
+# mv dist/* ../.
+# git rm -rf app config tests
+# git rm -rf bower.json package.json testem.json
+# git rm -rf .bowerrc .editorconfig .jshintrc .travis.yml
+# rm -rf dist
+# cd ..
+# git add .
+# git commit -m "Publishing to github pages"
+# # git push origin gh-pages
+# git checkout master
