91169314a53ecb1cd6c19b7346dec25d1a0404ec
zhaoyongqiang <zhyq0826@gmail.com>
2017-03-11 16:13:45 +0800

    update

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-# git branch -D gh-pages
-# git push origin --delete gh-pages
-# git checkout -b gh-pages
+git branch -D gh-pages
+git push origin --delete gh-pages
+git checkout -b gh-pages
 cd location-demo 
-pwd
 ember build --environment gh-pages
-# mv dist/* ../.
-# git rm -rf app config tests
-# git rm -rf bower.json package.json testem.json
-# git rm -rf .bowerrc .editorconfig .jshintrc .travis.yml
-# rm -rf dist
-# cd ..
-# git add .
-# git commit -m "Publishing to github pages"
-# # git push origin gh-pages
-# git checkout master
+mv dist/* ../.
+git rm -rf app config tests
+git rm -rf bower.json package.json testem.json
+git rm -rf .bowerrc .editorconfig .jshintrc .travis.yml
+rm -rf dist
+cd ..
+git add .
+git commit -m "Publishing to github pages"
+git push origin gh-pages
+git checkout master
