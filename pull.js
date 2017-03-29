708256a5a29104fb5bac7cdd397f02ea3b05c28d
zhaoyongqiang <zhyq0826@gmail.com>
2017-03-29 22:43:06 +0800

    update

--- pull.js
+++ pull.js
@@ -4,17 +4,17 @@ provinceNode.forEach(function(i, index){
     var length = provinceArray.length;
     if(length === 0){
         provinceArray.push({
-            "id": i.outerText,
+            "id": i.outerText.trim(),
             "name": ''
         })
     }else if(!provinceArray[length-1].name){
         if(i.outerText.trim()){
-            provinceArray[length-1].name = i.outerText;
+            provinceArray[length-1].name = i.outerText.trim();
         }
     }else {
         if(i.outerText.trim()){
             provinceArray.push({
-                "id": i.outerText,
+                "id": i.outerText.trim(),
                 "name": ''
             })
         }
@@ -45,3 +45,34 @@ cityNode.forEach(function(i, index){
         }
     }
 });
+
+(function(console){
+
+console.save = function(data, filename){
+
+    if(!data) {
+        console.error('Console.save: No data')
+        return;
+    }
+
+    if(!filename) filename = 'console.json'
+
+    if(typeof data === "object"){
+        data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], {type: 'text/json'}),
+        e    = document.createEvent('MouseEvents'),
+        a    = document.createElement('a')
+
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+ }
+})(console)
+
+
+console.save(JSON.stringify(provinceArray), 'province.json')
+console.save(JSON.stringify(cityArray), 'city.json')
